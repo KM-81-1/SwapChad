1543b462493ee478dce757c355473600371e1e12
pkononenko <pashakononenko@gmail.com>
2021-12-12 11:49:16 +0200

    mandatory structure change

---
+++ app_routes.py
@@ -0,0 +1,69 @@
+from aiohttp import web
+from auth import Auth, authorize
+
+import db
+
+
+# routes class
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def landing_page(_request):
+    return web.Response(text="Hello World!!!")
+
+
+@routes.post('/signup')
+async def signup(request):
+    data = await request.json()
+
+    # Obtaining registration information from the request
+    try:
+        user_info = data["user_info"]
+        credentials = data["credentials"]
+        username = credentials["username"]
+        password = credentials["password"]
+    except KeyError:
+        raise web.HTTPBadRequest
+
+    # Registering user
+    try:
+        session = db.get_session(request)
+        async with session.begin():
+            token = await Auth.signup(session, username, password, displayed_name=user_info["displayed_name"])
+    except Auth.UsernameIsTakenError:
+        raise web.HTTPUnprocessableEntity
+
+    # Sending JWT token to the user
+    return web.json_response({'token': token})
+
+
+@routes.post('/login')
+async def login(request):
+    data = await request.json()
+
+    # Obtaining login information from the request
+    try:
+        username = data["username"]
+        password = data["password"]
+    except KeyError:
+        raise web.HTTPBadRequest
+
+    # Logging user in
+    try:
+        session = db.get_session(request)
+        async with session.begin():
+            token = await Auth.login(session, username, password)
+    except Auth.UsernameIsTakenError:
+        raise web.HTTPBadRequest
+
+    # Sending JWT Bearer token to the user
+    return web.json_response({'token': token})
+
+
+@routes.post('/profile/modify')
+@authorize
+async def modify_profile(request):
+    data = await request.json()
+    print("Request from the user %s, authorized via token" % request["user_id"])
+    return web.json_response("Authorized")
